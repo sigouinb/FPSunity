bdb7acca3aa6952bb7fe57894a8167404d31c473
sigouinb <sigouinb@educ.college-montreal.qc.ca>
2024-05-10 15:13:13 -0400

    Initial commit

---
+++ Assets/Scripts/Helper Scripts/TagHolder.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Axis {
+    public const string HORIZONTAL = "Horizontal";
+    public const string VERTICAL = "Vertical";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Assets/Scripts/Player Script/PlayerSprintAndCrouch.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSprintAndCrouch : MonoBehaviour
+{
+    private PlayerMovement playerMovement;
+
+    public float sprint_Speed = 10f;
+    public float move_Speed = 5f;
+    public float crouch_Speed = 2f;
+
+    private Transform look_Root;
+    private float stand_Height = 1.6f;
+    private float crouch_Height = 1f;
+
+    private bool is_Crouching;
+
+
+    void Awake()
+    {
+        playerMovement = GetComponent<PlayerMovement>();
+
+        look_Root = transform.GetChild(0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Sprint();
+    }
+
+    void Sprint() {
+        if(Input.GetKeyDown(KeyCode.LeftShift) && !is_Crouching) {
+            playerMovement.speed = sprint_Speed;
+        }
+
+        if(Input.GetKeyUp(KeyCode.LeftShift) && !is_Crouching) {
+            playerMovement.speed = move_Speed;
+        }
+    } // sprint
+
+    void Crouch() {
+        if(Input.GetKeyDown(KeyCode.C)) {
+            // if we are crouching - stand up
+            if(is_Crouching) {
+                look_Root.localPosition = new Vector3(0f, stand_Height, 0f);
+                playerMovement.speed = move_Speed;
+
+                is_Crouching = false;
+
+
+            } else {
+                // if we are not crouching - stand up
+                look_Root.localPosition = new Vector3(0f, crouch_Height, 0f);
+                playerMovement.speed = crouch_Speed;
+
+                is_Crouching = true;
+            }
+        } // if we press C
+    } // crouch
+} // class
